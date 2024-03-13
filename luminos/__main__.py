30837b2d7964165ed14ea91d1670f521ff672f80
Ben Baptist <sawham6@gmail.com>
2024-03-13 00:51:25 -0400

    main organization

--- luminos/__main__.py
+++ luminos/__main__.py
@@ -1,29 +1,34 @@
 from luminos.core import Core
-
 import os
 import sys
 import time
 
+class Main:
+    def __init__(self):
+        self.core = Core()
+
+    def start(self):
+        if len(sys.argv) > 1:
+            target_dir = sys.argv[1]
+            os.chdir(target_dir)
+
+        while True:
+            try:
+                prompt = input("<user> ")
+            except EOFError:
+                print("")
+                continue
+            except KeyboardInterrupt:
+                break
+
+            with open(".luminos_history", "a") as f:
+                f.write(f"{time.time()} {prompt}\n")
+
+            self.core.run_llm(prompt)  # Utilize the run_llm method of core instance
+
 def main():
-    core_instance = Core()  # Initialize an instance of the Core class
-
-    if len(sys.argv) > 1:
-        target_dir = sys.argv[1]
-        os.chdir(target_dir)
-        
-    while True:
-        try:
-            prompt = input("<user> ")
-        except EOFError:
-            print("")
-            continue
-        except KeyboardInterrupt:
-            break
-
-        with open(".luminos_history", "a") as f:
-            f.write(f"{time.time()} {prompt}\n")
-
-        core_instance.run_llm(prompt)  # Utilize the run_llm method of Core instance
+    app = Main()
+    app.start()
 
 if __name__ == "__main__":
     main()
