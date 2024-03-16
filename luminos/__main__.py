ff88a16c5f8fb2f02ba9face47c416d3112266e2
Ben Baptist <sawham6@gmail.com>
2024-03-16 17:32:35 -0400

    Reintegrate Click for enhanced CLI argument handling in __main__.py.

--- luminos/__main__.py
+++ luminos/__main__.py
@@ -1,32 +1,47 @@
 import click
 from luminos.core import Core
 import os
+import sys
 import time
 
-@click.command()
-@click.option('--always-grant-permission', is_flag=True, help='Automatically grant permission for all safe operations.')
-@click.argument('directory', required=False, default='.')
-def main(always_grant_permiss... ioni, directory):
-    if always_grant_permission:
-        os.environ['ALWAYS_GRANT_PERMISSION'] = '1'
-    else:
-        os.environ['ALWAYS_GRANT_PERMISSION'] = '0'
+class Main:
+    def __init__(self):
+        self.core = Core()
 
-    if directory:
-        os.chdir(directory)
+    def start(self, always_grant_permission, directory):
+        # Apply the always grant permission setup
+        if always_grant_permission:
+            os.environ['ALWAYS_GRANT_PERMISSION'] = '1'
+        else:
+            os.environ['ALWAYS_GRANT_PERMISSION'] = '0'
 
-    core = Core()
+        # Change to the specified directory if provided
+        if directory:
+            os.chdir(directory)
 
-    while True:
-        try:
-            prompt = input("<user> ")
-        except (EOFError, Ke...  ypboardInterrupt):
-            break
+        while True:
+            try:
+                prompt = input("<user> ")
+            except EOFError:
+                print("")
+                continue
+            except KeyboardInterrupt:
+                break
 
-        with open(".luminos_history", "a") as f:
-            f.write(f"{time.time()} {prompt}\n")
+            with open(".luminos_history", "a") as f:
+                f.write(f"{time.time()} {prompt}\n")
 
-        core.run_llm(prompt)
+            self.core.run_llm(prompt)
+
+def main():
+    @click.command()
+    @click.option('--always-grant-permission', is_flag=True, default=False, help='Automatically grant permission for all safe operations.')
+    @click.argument('directory', required=False, type=click.Path(exists=True, file_okay=False))
+    def cli(always_grant_permission, directory='.'):
+        app = Main()
+        app.start(always_grant_permission, directory)
+
+    cli()
 
 if __name__ == "__main__":
     main()
