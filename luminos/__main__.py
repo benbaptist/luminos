12b075bf546121317de281ed71dd4c3551783f2a
Ben Baptist <sawham6@gmail.com>
2024-04-03 14:39:17 -0400

    Refactor UX and input handling into input.py and resolve NameError issue

--- luminos/__main__.py
+++ luminos/__main__.py
@@ -1,77 +1,15 @@
 from luminos.logic import Logic
-from luminos.input import get_user_input, style_normal, style_warning
+from luminos.input import get_user_input, start_user_interaction
 
 import click
-import os
-import signal
-import readline
-from prompt_toolkit import print_formatted_text
-from prompt_toolkit.formatted_text import FormattedText
-from prompt_toolkit.styles import Style
-
-style = Style.from_dict({
-    'response': 'ansiwhite', # White text for response
-    'model': 'ansigreen', # Green text for model name
-})
 
 class Main:
     def __init__(self):
         self.logic = Logic()
-        self.exit_signal_count = 0
-        signal.signal(signal.SIGINT, self.handle_sigint)
-
-    def handle_sigint(self, signum, frame):
-        if self.exit_signal_count == 0:
-            self.exit_signal_count += 1
-            print('\nPress Ctrl+C again to exit...')
-        else:
-            print('\nExiting...')
-            exit()
 
     def start(self, permissive, directory):
-        # Apply the permissive setup
-        if permissive:
-            warning_message = FormattedText([
-                ('class:warning', 'WARNING: You have enabled \"permissive\" mode. This provides the LLM with unprompted privileged access, which can pose potential security risks. To proceed, type \"YES\": '),
-            ])
-            print_formatted_text(warning_message, style=style_warning)
-            user_response = prompt('>', style=style_warning)
-            if user_response != 'YES':
-                print('Operation cancelled. Exiting...')
-                return
-            os.environ['ALWAYS_GRANT_PERMISSION'] = '1'
+        start_user_interaction(permissive, directory, self.logic)
 
-            current_style = style_warning
-        else:
-            os.environ['ALWAYS_GRANT_PERMISSION'] = '0'
-
-            current_style = style_normal
-
-        # Change to the specified directory if provided
-        if directory:
-            os.chdir(directory)
-
-        while True:
-            try:
-                cwd = os.getcwd()
-
-                if len(cwd) > 20:
-                    display_cwd = '...' + cwd[-17:]
-                else:
-                    display_cwd = cwd
-                    
-                user_input = get_user_input(style=current_style, display_cwd=display_cwd)
-                response = self.logic.generate_response(user_input)
-                formatted_response = FormattedText([
-                    ('class:response', response['message'] + '\n'),
-                    ('class:model', 'Model: ' + response['model'])
-                ])
-                print_formatted_text(formatted_response, style=style)
-            except EOFError:
-                print("\nExiting...")
-                break
-            except KeyboardInterrupt:
-                continue
 
 def main():
     @click.command()
--- luminos/input.py
+++ luminos/input.py
@@ -7,6 +7,8 @@ import readline
 import os
 import rlcompleter
 import atexit
+import signal
+from luminos.logic import Logic
 
 bindings = KeyBindings()
 
@@ -17,6 +19,11 @@ style_normal = Style.from_dict({
 style_warning = Style.from_dict({
     'prompt': 'ansired bold',
     'warning': 'bg:ansired ansiwhite bold',
+})
+
+style_response = Style.from_dict({
+    'response': 'ansiwhite',
+    'model': 'ansigreen',
 })
 
 readline.parse_and_bind('tab: complete')
@@ -51,3 +58,59 @@ def get_user_input(style, display_cwd):
     while not user_input.strip():
         user_input = prompt(f"[user@luminos {display_cwd}]$ ", style=style, key_bindings=bindings)
     return user_input
+
+def start_user_interaction(permissive, directory, logic):
+    exit_signal_count = 0
+    def handle_sigint(signum, frame):
+        nonlocal exit_signal_count
+        if exit_signal_count == 0:
+            exit_signal_count += 1
+            print('\nPress Ctrl+C again to exit...')
+        else:
+            print('\nExiting...')
+            exit()
+    signal.signal(signal.SIGINT, handle_sigint)
+    
+    # Apply the permissive setup
+    if permissive:
+        warning_message = FormattedText([
+            ('class:warning', 'WARNING: You have enabled \"permissive\" mode. This provides the LLM with unprompted privileged access, which can pose potential security risks. To proceed, type \"YES\": '),
+        ])
+        print_formatted_text(warning_message, style=style_warning)
+        user_response = prompt('>', style=style_warning)
+        if user_response != 'YES':
+            print('Operation cancelled. Exiting...')
+            return
+        os.environ['ALWAYS_GRANT_PERMISSION'] = '1'
+
+        current_style = style_warning
+    else:
+        os.environ['ALWAYS_GRANT_PERMISSION'] = '0'
+
+        current_style = style_normal
+    
+    # Change to the specified directory if provided
+    if directory:
+        os.chdir(directory)
+
+    while True:
+        try:
+            cwd = os.getcwd()
+
+            if len(cwd) > 20:
+                display_cwd = '...' + cwd[-17:]
+            else:
+                display_cwd = cwd
+                
+            user_input = get_user_input(current_style, display_cwd)
+            response = logic.generate_response(user_input)
+            formatted_response = FormattedText([
+                ('class:response', response['message'] + '\n'),
+                ('class:model', 'Model: ' + response['model'])
+            ])
+            print_formatted_text(formatted_response, style=style_response)
+        except EOFError:
+            print("\nExiting...")
+            break
+        except KeyboardInterrupt:
+            continue
