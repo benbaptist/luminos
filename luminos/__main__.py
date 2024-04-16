d6f7385c2c1b152bc6249d8d95c4c38a33de9989
Ben Baptist <sawham6@gmail.com>
2024-04-15 23:15:23 -0400

    Revert "Add all changes"
    This reverts commit 4278ef612b494eb8381d94a6dcf3f9cac884651b.

--- luminos/__main__.py
+++ luminos/__main__.py
@@ -1,25 +1,27 @@
-"""Entry point for Luminos CLI"""
+from luminos.logic import Logic
+from luminos.config import Config
+from luminos.input import get_user_input, start_user_interaction
 
 import click
-from luminos.config import Config
-from luminos.logic import Logic
-from luminos.input import Input
 
-class LuminosCLI:
+class Main:
     def __init__(self):
         self.config = Config()
         self.logic = Logic(self)
-        self.input = Input(self.logic)
 
-    def run(self, permissive, directory):
-        self.input.start_interaction(permissive, directory)
+    def start(self, permissive, directory):
+        start_user_interaction(permissive, directory, self.logic)
 
-@click.command()
-@click.option('--permissive', is_flag=True, default=False, help='Automatically grant permission for all safe operations.')
-@click.argument('directory', required=False, type=click.Path(exists=True, file_okay=False))
-def main(permissive, directory='.'):
-    cli = LuminosCLI()
-    cli.run(permissive, directory)
+
+def main():
+    @click.command()
+    @click.option('--permissive', is_flag=True, default=False, help='Automatically grant permission for all safe operations.')
+    @click.argument('directory', required=False, type=click.Path(exists=True, file_okay=False))
+    def cli(permissive, directory='.'):
+        app = Main()
+        app.start(permissive, directory)
+
+    cli()
 
 if __name__ == "__main__":
     main()
