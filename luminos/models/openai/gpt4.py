39219d33755893093b77234e09050d8721c64262
Ben Baptist <sawham6@gmail.com>
2024-04-22 16:59:33 -0400

    updated to use gpt-4-turbo

--- luminos/models/openai/gpt4.py
+++ luminos/models/openai/gpt4.py
@@ -2,4 +2,4 @@ from luminos.models.openai.base_openai import BaseOpenAI
 
 class GPT4(BaseOpenAI):
     def __init__(self, api_key: str):
-        super().__init__(api_key, model="gpt-4-0125-preview")
+        super().__init__(api_key, model="gpt-4-turbo")
