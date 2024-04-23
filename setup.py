05aa59d2c3b260c9a9043e6fbd28e94eed5db669
Ben Baptist <sawham6@gmail.com>
2024-04-23 15:19:12 -0400

    added reqs to setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,13 @@ setup(
     packages=find_packages(),
     install_requires=[
         'requests',
-        'docstring_parser'
+        'docstring_parser',
+        'litellm',
+        'colorlog',
+        'prompt_toolkit==3.0.43',
+        'anthropic>=0.25.6',
+        'openai>=1.0',
+        'click>=8.1.7'
     ],
     python_requires='>=3.6',
     author='Ben Baptist',
@@ -24,12 +30,12 @@ setup(
         "Intended Audience :: Developers",
         "License :: OSI Approved :: MIT License",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.6"
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
         "Topic :: Software Development :: Libraries",
         "Operating System :: POSIX :: Linux",
-    ],
+    ]
 )
