b38172c7b6f15f3e88bc9126d3aefabaf766f518
Ben Baptist <sawham6@gmail.com>
2024-05-19 02:05:54 -0400

    Bump version to 0.13.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='luminos',
-    version='0.13.0',
+    version='0.13.1',
     packages=find_packages(),
     install_requires=[
         'requests',
